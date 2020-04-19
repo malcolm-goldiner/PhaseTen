acc9e7e31d3ee93612c4d16a7451d8626476d0c5
Malcolm Goldiner <malcolmgoldiner@gmail.com>
2020-04-19 15:32:38 -0700

    - add custom datasource in for collection move - move cards between decks by tapping on them - unused boiler plate drag and drop code added from tutorial

--- PhaseTen/Phase10GameViewController.swift
+++ PhaseTen/Phase10GameViewController.swift
@@ -9,6 +9,76 @@
 import UIKit
 import Combine
 
+enum DeckType {
+    case hand
+    case set
+}
+
+class Phase10GameViewDataSource: NSObject, UICollectionViewDataSource {
+    
+    private var player: Phase10Player?
+    
+    var deckType: DeckType
+    
+    init(deckType: DeckType, game: Phase10GameEngine) {
+        self.deckType = deckType
+        self.player = game.players.first
+    }
+    
+    func cardAt(_ indexPath: IndexPath) -> Phase10Card? {
+        if deckType == .hand {
+            return player?.hand[indexPath.row]
+        } else {
+            return player?.potentialSets[indexPath.row]
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if deckType == .set {
+            return player?.potentialSets.count ?? 0
+        } else {
+            return player?.hand.count ?? 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.reuseIdenitifer, for: indexPath) as? CardCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.card = cardAt(indexPath)
+        
+        return cell
+    }
+    
+    func addCard(_ card: Phase10Card, atIndexPath indexPath: IndexPath) {
+        switch deckType {
+        case .set:
+            player?.potentialSets.append(card)
+        case .hand:
+            player?.hand.append(card)
+        }
+    }
+    
+    func moveCard(at sourceIndex: Int, to destinationIndex: Int?) {
+        guard sourceIndex != destinationIndex else { return }
+        
+        if let card = cardAt(IndexPath(row: sourceIndex, section: 0)) {
+            switch deckType {
+            case .hand:
+                player?.potentialSets.append(card)
+                player?.hand.remove(at: sourceIndex)
+            case .set:
+                player?.hand.append(card)
+                player?.potentialSets.remove(at: sourceIndex)
+                
+            }
+        }
+    }
+    
+    
+}
+
 class Phase10GameViewController: UIViewController {
     
     @IBOutlet weak var scoreLabel: UILabel!
@@ -37,10 +107,13 @@ class Phase10GameViewController: UIViewController {
         }
     }
     
+    var handDataSource: Phase10GameViewDataSource?
+    
+    var setDataSource: Phase10GameViewDataSource?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupCollectionViews()
         startGame()
         listenForGameStateChanges()
     }
@@ -63,7 +136,7 @@ class Phase10GameViewController: UIViewController {
         _ = player?.$hand.sink(receiveValue: { [weak self] _ in
             self?.needsReload = true
         })
-    
+        
         _ = player?.$potentialSets.sink(receiveValue: { [weak self] _ in
             self?.needsReload = true
         })
@@ -73,8 +146,12 @@ class Phase10GameViewController: UIViewController {
     private func startGame() {
         Phase10GameEngine.shared.addPlayer()
         player = Phase10GameEngine.shared.players.first
-        currentHandCollectionView.reloadData()
+        handDataSource = Phase10GameViewDataSource(deckType: .hand, game: Phase10GameEngine.shared)
+        setDataSource = Phase10GameViewDataSource(deckType: .set, game: Phase10GameEngine.shared)
         
+        setupCollectionViews()
+        currentHandCollectionView.reloadData()
+        potentialCardSetCollectionView.reloadData()
     }
     
     private func setupCollectionViews() {
@@ -83,37 +160,134 @@ class Phase10GameViewController: UIViewController {
         self.potentialCardSetCollectionView.register(UINib(nibName: CardCollectionViewCell.nibName, bundle: Bundle.main), forCellWithReuseIdentifier: CardCollectionViewCell.reuseIdenitifer)
         
         currentHandCollectionView.delegate = self
-        currentHandCollectionView.dataSource = self
+        currentHandCollectionView.dataSource = handDataSource
         
         potentialCardSetCollectionView.delegate = self
-        potentialCardSetCollectionView.dataSource = self
+        potentialCardSetCollectionView.dataSource = setDataSource
+        
+        currentHandCollectionView.dragDelegate = self
+        currentHandCollectionView.dropDelegate = self
+        
+        potentialCardSetCollectionView.dropDelegate = self
+        potentialCardSetCollectionView.dragDelegate = self
+        
+        potentialCardSetCollectionView.reloadData()
+        currentHandCollectionView.reloadData()
     }
     
 }
 
-extension Phase10GameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-   
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == potentialCardSetCollectionView {
-            return player?.potentialSets.count ?? 0
-        }
-        
-        return player?.hand.count ?? 0
-    }
+extension Phase10GameViewController: UICollectionViewDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.reuseIdenitifer, for: indexPath) as? CardCollectionViewCell else {
-            return UICollectionViewCell()
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           guard let dataSource = collectionView.dataSource as? Phase10GameViewDataSource,
+               dataSource.cardAt(indexPath) != nil else {
+               return
+           }
+           
+           dataSource.moveCard(at: indexPath.row, to: nil)
+           currentHandCollectionView.reloadData()
+           potentialCardSetCollectionView.reloadData()
+       }
+    
+}
+
+extension Phase10GameViewController: UICollectionViewDropDelegate, UICollectionViewDragDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        guard let dataSource = collectionView.dataSource as? Phase10GameViewDataSource else {
+            return
         }
         
-        if collectionView == currentHandCollectionView {
-               cell.card = player?.hand[indexPath.row]
+        let destinationIndexPath: IndexPath
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
         } else {
-            cell.card = player?.potentialSets[indexPath.row]
+            destinationIndexPath = IndexPath(
+                item: collectionView.numberOfItems(inSection: 0),
+                section: 0)
         }
         
-        return cell
+        let item = coordinator.items[0]
+        
+        switch coordinator.proposal.operation
+        {
+        case .move:
+            print("Moving...")
+            // 1
+            if let sourceIndexPath = item.sourceIndexPath {
+                // 2
+                collectionView.performBatchUpdates({
+                    dataSource.moveCard(
+                        at: sourceIndexPath.item,
+                        to: destinationIndexPath.item)
+                    collectionView.deleteItems(at: [sourceIndexPath])
+                    collectionView.insertItems(at: [destinationIndexPath])
+                })
+                // 3
+                coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+            }
+        case .copy:
+            print("Copying...")
+            let itemProvider = item.dragItem.itemProvider
+            
+            itemProvider.loadObject(ofClass: NSString.self) { string, error in
+                if let string = string as? String {
+                    
+                    let splitCardString = string.split(separator: " ")
+                    
+                    let color = splitCardString.first!.description
+                    if let parsedType = Int(splitCardString.last!.description),
+                        let type = Phase10CardType(rawValue: parsedType) {
+                        
+                        let card = Phase10Card(type, color: UIColor(named: color))
+                        
+                        dataSource.addCard(card, atIndexPath: destinationIndexPath)
+                        
+                        DispatchQueue.main.async {
+                            collectionView.insertItems(at: [destinationIndexPath])
+                        }
+                    }
+                    
+                }
+            }
+        default:
+            return
+        }
     }
     
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        if let itemForDrag =  collectionView == currentHandCollectionView ? player?.hand[indexPath.row] : player?.potentialSets[indexPath.row] {
+            let itemProvider = NSItemProvider(object: "\(String(describing: itemForDrag.color)) \(itemForDrag.type)" as NSItemProviderWriting)
+            let dragItem = UIDragItem(itemProvider: itemProvider)
+            return [dragItem]
+        }
+        
+        return []
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        dropSessionDidUpdate session: UIDropSession,
+        withDestinationIndexPath destinationIndexPath: IndexPath?
+    ) -> UICollectionViewDropProposal {
+        if session.localDragSession != nil {
+            guard session.items.count == 1 else {
+                return UICollectionViewDropProposal(operation: .cancel)
+            }
+            
+            if collectionView.hasActiveDrag {
+                return UICollectionViewDropProposal(operation: .move,
+                                                    intent: .insertAtDestinationIndexPath)
+            } else {
+                return UICollectionViewDropProposal(operation: .copy,
+                                                    intent: .insertAtDestinationIndexPath)
+            }
+        } else {
+            return UICollectionViewDropProposal(
+                operation: .forbidden)
+        }
+    }
     
 }
