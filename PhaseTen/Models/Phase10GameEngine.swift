3b334008b806c4d1d0f4861d5a9c0c83ca8ab291
Malcolm Goldiner <malcolmgoldiner@gmail.com>
2020-07-04 19:58:00 -0700

    - Fully comment and document Phase10GameEngine

--- PhaseTen/Models/Phase10GameEngine.swift
+++ PhaseTen/Models/Phase10GameEngine.swift
@@ -6,20 +6,23 @@
 //  Copyright © 2020 Malcolm Goldiner. All rights reserved.
 //
 
-import Foundation
 import Combine
 import CloudKit
 
 /// Manager of game state (decks, cards, scores) for up to 6 players governed by the official Phase10 Rules
 class Phase10GameEngine: Phase10Model {
     
-    /// Shared Game Engine repesenting the CloudKit record entry for the current game
-    static let shared = Phase10GameEngine()
-    
     // MARK: - Phase10Model
     
     /// String representing CloudKit Table name for the game entity
     static let recordType = "Phase10Game"
+    
+    // MARK: - Static Variables
+    
+    /// Shared Game Engine repesenting the CloudKit record entry for the current game
+    static let shared = Phase10GameEngine()
+    
+    // MARK: - CloudKit Field Keys
     
     /// CloudKit field names contained within Phase10GameTable
     enum Key: String {
@@ -31,10 +34,13 @@ class Phase10GameEngine: Phase10Model {
     
     // MARK: - Constant Official Game Rules
     
+    /// Default number of cards per Phase10 common rules
     private static let handSize = 10
     
+    /// Max number of players per Phase10 common rules
     private static let maxPlayers = 6
     
+    /// Most common Phases per Phase10 common rules
     static let generalReqs: [Phase] = [
         .one(requirements:[.numberOf(count: 2, combos: .setOf(count: 3))]),
         .two(requirements: [.setOf(count: 3), .runOf(count: 4)]),
@@ -48,7 +54,7 @@ class Phase10GameEngine: Phase10Model {
         .ten(requirements: [.setOf(count: 5), .setOf(count: 3)])
     ]
     
-    // MARK: - Public Game State Variables
+    // MARK: - Observed Public Game State Variables
     
     /// Main game deck of cards that all hands are dealt from
     @Published
@@ -74,16 +80,28 @@ class Phase10GameEngine: Phase10Model {
     @Published
     var turnIndex: Int = 0
     
-    /// Phase10Turn keeping track of the cards picked up and discarded during this turn
-    var currentTurn: Phase10Turn?
+    /// Number of open CloudKit load requests
+    @Published
+    var openReqs = -1
     
-    var winningPlayerIndex: Int?
-    
-    var localPlayer: Phase10Player?
-    
+    /// Number corresponding to invited players used and entered only on game owning device so that players cant be dealt in.
     @Published
     var expectedNumberOfPlayers: Int?
     
+    // MARK: - Public Game State
+    
+    /// Phase10Turn keeping track of the cards picked up and discarded during this turn
+    var currentTurn: Phase10Turn?
+    
+    /// Index in the Phase10Game's players array of the winning player
+    var winningPlayerIndex: Int?
+    
+    /// The player corresponding to this current device and signed in iCloud Account
+    var localPlayer: Phase10Player?
+    
+    // MARK: - Private Variables
+    
+    /// Only the GameOwner can deal everyone in, this returns turn when all players have joined and that message is recieved on the game owner's device.
     private var needToDeal: Bool = false {
         didSet {
             if needToDeal && Phase10GameEngineManager.shared.isGameOwner
@@ -93,6 +111,21 @@ class Phase10GameEngine: Phase10Model {
                 }
             }
         }
+    }
+    
+    /// Card sorting function going by the placeIndex in the database (This should preserve order when decks are downloaded async)
+    private let cardSort: (Phase10Card, Phase10Card) -> Bool = { (left, right) -> Bool in
+        guard let leftPlace = left.placeIndex,
+            let rightPlace = right.placeIndex else {
+                return false
+        }
+        
+        return leftPlace < rightPlace
+    }
+    
+    /// Player sorting function going by the saved index in the database (this should preserve order when players are downloaded async)
+    private let playerSort: (Phase10Player, Phase10Player) -> Bool = { (left, right) -> Bool in
+        return left.index < right.index
     }
     
     // MARK: - Initailzers
@@ -112,7 +145,7 @@ class Phase10GameEngine: Phase10Model {
         $openReqs.combineLatest($expectedNumberOfPlayers).map { [weak self] (reqs, expectedPlayers) in reqs == 0 && expectedPlayers == self?.players.count }.subscribe(openReqSub)
     }
     
-    // MARK: - Game Logic
+    // MARK: - Public Game Logic Functions
     
     /**
         Moves players in Phase 1...8 to the next phase, ends the game for a player on phase 9
@@ -138,6 +171,11 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
+    /**
+     Records either a pickup or discard action to the local players turn to keep track of whether they have completed their turn or not.
+     
+     - Parameter action: Phase10Action containing a card discard or pickup
+     */
     func addActionToTurn(_ action: Phase10Action) {
         if currentTurn == nil {
             currentTurn = Phase10Turn()
@@ -151,19 +189,24 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
+    /// Increments the scores of all players by the remaining cards in their hand, discards those cards, and calls out to deal in each player for the next round.
     func beginNewRound() {
         for player in players {
-            discardPile.insert(contentsOf: player.hand, at: 0)
-            player.hand = []
-            beginRoundForPlayer(player)
-            
             if player != winningPlayer {
                 // not here
                 scoresByPlayer[player, default: 0] += player.hand.reduce(0) { $0 + $1.type.rawValue }
             }
+            
+            // probably need to shuffle the discard pile and deck
+            discardPile.insert(contentsOf: player.hand, at: 0)
+            player.hand = []
+            beginRoundForPlayer(player)
         }
     }
     
+    
+    /// Adds the local player to the shared Phase10GameEngine's array of players if we are below the maximum number of players.
+    ///  Sets as the game owner if the local devices iCloud account created the game.
     func addPlayer() {
         guard players.count < Phase10GameEngine.maxPlayers - 1 else {
             print("Can't add anymore players")
@@ -177,21 +220,15 @@ class Phase10GameEngine: Phase10Model {
         players.append(newPlayer)
     }
     
-    private func beginRoundForPlayer(_ player: Phase10Player) {
-        if player.phase == nil {
-            player.phase = Phase10GameEngine.generalReqs.first
-        }
-        
-        if shouldDeal() {
-            player.hand.append(contentsOf: deck.cards[0...9])
-            deck.cards.removeSubrange(0...9)
-        }
-    }
+    // MARK: - Public Game Validation Functions
     
-    private func shouldDeal() -> Bool {
-        return Phase10GameEngineManager.shared.isGameOwner && players.count == expectedNumberOfPlayers
-    }
-    
+    /**
+     Returns true if the input array of cards matches the requirement for the input player's current Phase
+     
+     - Parameters:
+        - player: Phase10Player who played the input cards whose current Phase is being validated
+        - playedCards: Arrays (should be 2) of Phase10Cards being checked against the requirements for the input player's current Phase.
+     */
     func isPhaseCleared(for player: Phase10Player,
                         playedCards: [[Phase10Card]]) -> Bool {
         var result = false
@@ -224,21 +261,36 @@ class Phase10GameEngine: Phase10Model {
         return result
     }
     
+    /**
+     Returns true when the input cards match the requirements for the input players current Phase10Phase according to the current game rules.
+     If they do match, this player is moved to the next phase, or the game will end if this player has achieved Phase 10
+     
+     - Parameters:
+        - player: Phase10Player who played the input cards
+        - playedCards: Array of Phase10Cards to be checked against the requirements for the input player's current Phase
+     */
     func validatePhase(for player: Phase10Player, playedCards: [[Phase10Card]]) -> Bool {
         let result = isPhaseCleared(for: player, playedCards: playedCards)
         if result {
             movePlayerToNextPhase(player)
             discardPile.append(contentsOf: playedCards.first!)
             discardPile.append(contentsOf: playedCards.last!)
-            winningPlayer = player
+            
+            if case Phase.ten(requirements: _)? = player.phase {
+                winningPlayer = player
+            }
         }
         
         return result
     }
     
-    @Published
-    var openReqs = -1
+    // MARK: - CloudKit Sync
     
+    /**
+     Sets the local shared Phase10GameEngine's recordID to the Game record from the public iCloud database that matches the input gameID.
+     
+     - Parameter gameID: String corresponding to the recordName for the Phase10Game that should be returned
+     */
     func loadGame(for gameID: String) {
         let reference = CKRecord.Reference(recordID: CKRecord.ID(recordName: gameID), action: .none)
         
@@ -264,10 +316,11 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
-    let playerSort: (Phase10Player, Phase10Player) -> Bool = { (left, right) -> Bool in
-        return left.index < right.index
-    }
-    
+    /**
+     Sets the local shared Phase10GameEngine's players array to contain Players matching records from the public iCloud database that have the input gameID.
+     
+     - Parameter gameID: String corresponding to the recordName for the Phase10Game that owns the players that should be returned
+     */
     func loadPlayers(for gameID: String) {
         let reference = CKRecord.Reference(recordID: CKRecord.ID(recordName: gameID), action: .none)
         let pred = NSPredicate(format: "\(Phase10Player.Key.game) == %@", reference)
@@ -290,6 +343,11 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
+    /**
+     Sets the local shared Phase10GameEngine's deck to be the Deck record from the public iCloud database that matches the input gameID.
+     
+     - Parameter gameID: String corresponding to the recordName for the Phase10Game that owns the deck that should be returned
+     */
     func loadDeck(for gameID: String) {
         let reference = CKRecord.Reference(recordID: CKRecord.ID(recordName: gameID), action: .none)
         let pred = NSPredicate(format: "\(Phase10Deck.Key.game) == %@", reference)
@@ -312,6 +370,12 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
+    /**
+     Returns a Phase10Card object, where possible, by parsing the description in the input Record.
+     Ex: hand blue 9 --> a Phase10Card with color blue and number 9
+     
+     - Parameter record: CKRecord containing a description field that should be expanded to produce a Phase10Card
+     */
     func card(from record: CKRecord) -> Phase10Card? {
         if let description = record[.description] as? String {
             let cardDesc = CardDescriptor(desc: "hand \(description)")
@@ -331,6 +395,13 @@ class Phase10GameEngine: Phase10Model {
         return nil
     }
     
+    /**
+     Loads all the Card objects matching the input references from the public iCloud database into the input Phase10Deck
+     
+     - Parameters:
+        - deck: Phase10Deck that should have all the cards corresponding to the input References loaded into it (This deck corresponds to the record id that holds these cards in the database)
+        - references: Array of References to Phase10Cards that should be loaded into the input Phase10Deck
+     */
     func loadCards(into deck: Phase10Deck, withReferences references: [CKRecord.Reference]) {
         openReqs += references.count
         for reference in references {
@@ -346,15 +417,11 @@ class Phase10GameEngine: Phase10Model {
         deck.cards = deck.cards.sorted(by: cardSort)
     }
     
-    let cardSort: (Phase10Card, Phase10Card) -> Bool = { (left, right) -> Bool in
-        guard let leftPlace = left.placeIndex,
-            let rightPlace = right.placeIndex else {
-                return false
-        }
-        
-        return leftPlace < rightPlace
-    }
-    
+    /**
+     Loads all the Card objects matching the input references from the public iCloud database into the local Phase10GameEngine.shared discardPile
+     
+     - Parameter references: Array of References to Phase10Cards that should be loaded into the local shared Phase10GameEngine discard Pile
+     */
     func loadCardsIntoDiscardPile(withReferences references: [CKRecord.Reference]) {
         openReqs += references.count
         for reference in references {
@@ -369,6 +436,13 @@ class Phase10GameEngine: Phase10Model {
         discardPile = discardPile.sorted(by: cardSort)
     }
     
+    /**
+     Loads all the Card objects matching the input references from the public iCloud database into the input players hand array.
+     
+     - Parameters:
+        - player: Phase10Player who's hand of Phase10Cards should be loaded
+        - references: Array of references to Phase10Cards that should be loaded into the input players hand array
+     */
     func loadCards(for player: Phase10Player, withReferences references: [CKRecord.Reference]) {
         openReqs += references.count
         for reference in references {
@@ -382,4 +456,27 @@ class Phase10GameEngine: Phase10Model {
         }
     }
     
+    // MARK: - Private Functions
+    
+    /**
+     Starts a new round of the game by setting a player's hand with 10 new cards from the deck. At the beginning of the game this will also set all players to Phase 0.
+     This is expected to be called after 1 or more players has cleared the current Phase and a new round is beginning or directly at the beginning of the game.
+     
+     - Parameter player: The player who is starting a new round of the game
+     */
+    private func beginRoundForPlayer(_ player: Phase10Player) {
+        if player.phase == nil {
+            player.phase = Phase10GameEngine.generalReqs.first
+        }
+        
+        if shouldDeal() {
+            player.hand.append(contentsOf: deck.cards[0...9])
+            deck.cards.removeSubrange(0...9)
+        }
+    }
+    
+    // Returns true if this device is permitted to deal cards, this happens when all invited players have joined and this is called on the device that created the game
+    private func shouldDeal() -> Bool {
+        return Phase10GameEngineManager.shared.isGameOwner && players.count == expectedNumberOfPlayers
+    }
 }
